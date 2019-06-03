a591af2cc6001482e510474a571f45a084358cc0
Luke-Patterson <lsp52@cornell.edu>
2019-06-02 20:36:44 -0400

    update

--- MH_set_attrib.py
+++ MH_set_attrib.py
@@ -1,7 +1,10 @@
-# assign additional attributes to cards
+# assign attributes to MH cards
 import pandas as pd
-df = pd.read_excel('input/raw_MH_file_rankings.xlsx')
+from def_classes import Card
+from def_classes import Set
 
+MH = Set('input/raw_MH_file_rankings.xlsx')
+df = MH.df 
 # clean up text
 df['text'] = df.text.str.lower()
 df['text'] = df['text'].str.replace('\n',' ')
@@ -16,6 +19,7 @@ def TextContains(str):
 # creatures, or spells that are effectively creatures
 df['creature_like'] = False
 df.loc[IsCreature, 'creature_like'] = True
+df.loc[TextContains('token'), 'creature_like'] = True
 
 # evasive creatures (0-1 scale)
 df['evasive'] = 0
@@ -27,3 +31,11 @@ df.loc[IsCreature & TextContains("can't be blocked"), 'evasive'] = 1
 # creatures that conditionally gain flying get evasive .5
 df.loc[IsName('Chillerpillar'),'evasive'] = .5
 df.loc[IsName('Bogardan Dragonheart'),'evasive'] = .5
+
+# create a class for each creature and
+# write conditions for modifications to base score
+def load_attributes(row):
+    return(Card(row.to_dict()))
+df['Card_Obj'] = df.apply(load_attributes,axis=1)
+
+MH.df = df
--- def_classes.py
+++ def_classes.py
@@ -1,17 +1,42 @@
+import pandas as pd
+import numpy as np
+class Set():
+    def __init__(self, df_path):
+        self.df = pd.read_excel(df_path)
+
 class Card():
-    def __init__(self, **kwargs):
-        self.__dict__.update(kwargs)
-    def update(self, **kwargs):
-        self.__dict__.update(kwargs)
+    def __init__(self, attrib_dict):
+        self.__dict__.update(attrib_dict)
+
+    def update_attrib(self, attrib_dict):
+        self.__dict__.update(attrib_dict)
+
+    def update_score(self):
+        pass
 
 class Pool():
-    def __init__(self, **kwargs):
-        # grab 90 cards, sampled by rarity
+    def __init__(self, Set):
+        self.Set = Set
+        df = pd.DataFrame()
+        # grab 90 cards from set, sampled by rarity
         # 6 rares (1 in 8 chance replaced by mythic)
+        mythics = np.random.binomial(n=6,p=.125)
+        df = df.append(self.Set.df.loc[self.Set.df['rarity']=='rare',:]
+            .sample(6-mythics, replace=True))
+        df = df.append(self.Set.df.loc[self.Set.df['rarity']=='mythic',:]
+            .sample(mythics, replace=True))
         # 18 uncommons
-        # 
+        df = df.append(self.Set.df.loc[self.Set.df['rarity']=='uncommon',:]
+            .sample(18, replace=True))
+        # 60 commons
+        df = df.append(self.Set.df.loc[self.Set.df['rarity']=='uncommon',:]
+            .sample(60, replace=True))
+        # 6 basic snow-lands
+        df = df.append(self.Set.df.loc[self.Set.df['rarity']=='basic',:]
+            .sample(6, replace=True))
 
+        self.df = df
 
 class Deck():
-    def __init__(self, **kwargs):
-        pass
+    def __init__(self):
+        self.df = pd.DataFrame()
